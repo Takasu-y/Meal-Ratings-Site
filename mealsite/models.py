053d9c455399d85d4129a43e2de9ff3a778c69df
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-10 17:15:29 +0900

    add template list, detail

--- mealsite/models.py
+++ mealsite/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Meal(models.Model):
+    """Model definition for Meal."""
+
+    # TODO: Define fields here
+    # name =
+    # description =
+    # imageUrl =
+    # countryOfOrigin =
+    # typicalMealTime =
+    # dateAdded =
+
+    class Meta:
+        """Meta definition for Meal."""
+
+        verbose_name = 'Meal'
+        verbose_name_plural = 'Meals'
+
+    def __str__(self):
+        """Unicode representation of Meal."""
+        return self.name
+
+
+class MealRating(models.Model):
+    """Model definition for MealRating."""
+
+    # TODO: Define fields here
+    # meal =
+    # rating =
+    # dateOfRating =
+
+    class Meta:
+        """Meta definition for MealRating."""
+
+        verbose_name = 'MealRating'
+        verbose_name_plural = 'MealRatings'
+
+    def __str__(self):
+        """Unicode representation of MealRating."""
+        return self.rating
