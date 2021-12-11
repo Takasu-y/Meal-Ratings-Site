41d3cc1e1a5f6fae369229b64304c24374c76459
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-11 11:22:52 +0900

    fixtureの作成

--- mealsite/urls.py
+++ mealsite/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, MealListView, MealDetailView
 
 app_name='meal'
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('category/', MealListView.as_view(), name="mealList"),
+    # path('detail/', MealDetailView.as_view(), name="mealDetail"),
 
 ]
--- mealsite/views.py
+++ mealsite/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 
+from .models import Meal, MealRating
 # Create your views here.
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Meal
     template_name = "index.html"
+
+
+class MealListView(ListView):
+    model = Meal
+    template_name = "mealSite/categoryList.html"
+
+
+
+class MealDetailView(DetailView):
+    model = Meal
+    template_name = "mealSite/mealDetail.html"
 
