f3c78b1e801ad296e2f8e04ff4f315266acec7bc
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-17 18:52:12 +0900

    テンプレートの修正&IndexViewの修正

--- mealsite/urls.py
+++ mealsite/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import IndexView, MealListView, MealDetailView
+from .views import IndexView, MealDetailView, AddMealFormView, HistoryListView
 
 app_name='meal'
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('List/<str:category>/', MealListView.as_view(), name="mealList"),
+    # path('List/<str:category>/', MealListView.as_view(), name="mealList"),
     path('detail/<int:pk>/', MealDetailView.as_view(), name="mealDetail"),
+    path('add-meal-form/', AddMealFormView.as_view(), name="addMeal"),
+    path('see-history/', HistoryListView.as_view(), name="history"),
 
 ]
--- mealsite/views.py
+++ mealsite/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import ListView, DetailView, FormView, TemplateView
 from django.db.models import Avg
 from django.utils.timezone import make_aware
 from django.urls import reverse_lazy
@@ -9,48 +9,13 @@ from .models import Meal
 from .forms import MealFrom, MealRatingForm
 # Create your views here.
 
-class IndexView(ListView, FormView):
+class IndexView(ListView):
     model = Meal
-    form_class = MealFrom
-    success_url = None
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["topRate"] = Meal.objects.all().annotate(avg_rating = Avg("mealrating__rating")).filter(avg_rating__gte=3.5).order_by('-avg_rating')[0:3]
-        context["recently"] = Meal.objects.all().order_by('-dateAdded')[0:3]
-
-        return context
-
-    def form_valid(self, form):
-        form.save()
-        self.success_url = reverse_lazy("meal:index")
-        return super().form_valid(form)
-
-class MealListView(ListView):
-    model = Meal
-    template_name = "mealSite/categoryList.html"
     context_object_name = "meals"
 
     def get_queryset(self):
-        #objの取得
-        if self.kwargs["category"] == "morning":
-            meals = self.model.objects.filter(typicalMealTime=1)
-
-        elif self.kwargs["category"] == "afternoon":
-            meals = self.model.objects.filter(typicalMealTime=2)
-
-        elif self.kwargs["category"] == "evening":
-            meals = self.model.objects.filter(typicalMealTime=3)
-
-        elif self.kwargs["category"] == "recently":
-            now = make_aware(datetime.datetime.now())
-            borderElapsedDays = now + datetime.timedelta(days=-3000) #TODO:90日に変更予定
-            meals = self.model.objects.all().filter(dateAdded__gte=borderElapsedDays)
-
-        elif self.kwargs["category"] == "topRate":
-            borderRating = 3.5 #TODO:4.5へ変更予定
-            meals = self.model.objects.all().annotate(avg_rating = Avg("mealrating__rating")).filter(avg_rating__gte=borderRating)
+        meals = self.model.objects.all().order_by("-dateAdded")
 
         #取得したmeals objをqでソートする
         q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
@@ -61,15 +26,27 @@ class MealListView(ListView):
         elif q == "date":
             # new added
             meals = meals.order_by("-dateAdded")
-        else:
+        elif q == "country":
             meals = meals.order_by("countryOfOrigin")
 
         return meals
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["category"] = self.kwargs["category"]
+        context["count"] = self.model.objects.all().count()
+
         return context
+
+class AddMealFormView(FormView):
+    model = Meal
+    form_class = MealFrom
+    template_name = "mealSite/addMeal.html"
+
+
+class HistoryListView(TemplateView):
+    #TODO: make history model, change TemplateView -> ListView
+    # model = History
+    template_name = "mealSite/seeHistory.html"
 
 
 class MealDetailView(DetailView, FormView):
@@ -88,3 +65,47 @@ class MealDetailView(DetailView, FormView):
         self.success_url = reverse_lazy('meal:mealDetail', kwargs={ "pk": meal_id})
         return super().form_valid(form)
 
+
+# class MealListView(ListView):
+#     model = Meal
+#     template_name = "mealSite/categoryList.html"
+#     context_object_name = "meals"
+
+#     def get_queryset(self):
+#         #objの取得
+#         if self.kwargs["category"] == "morning":
+#             meals = self.model.objects.filter(typicalMealTime=1)
+
+#         elif self.kwargs["category"] == "afternoon":
+#             meals = self.model.objects.filter(typicalMealTime=2)
+
+#         elif self.kwargs["category"] == "evening":
+#             meals = self.model.objects.filter(typicalMealTime=3)
+
+#         elif self.kwargs["category"] == "recently":
+#             now = make_aware(datetime.datetime.now())
+#             borderElapsedDays = now + datetime.timedelta(days=-90) #TODO:90日に変更予定
+#             meals = self.model.objects.all().filter(dateAdded__gte=borderElapsedDays)
+
+#         elif self.kwargs["category"] == "topRate":
+#             borderRating = 3.5 #TODO:4.5へ変更予定
+#             meals = self.model.objects.all().annotate(avg_rating = Avg("mealrating__rating")).filter(avg_rating__gte=borderRating)
+
+#         #取得したmeals objをqでソートする
+#         q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
+
+#         if q == "rating":
+#             #popular meals
+#             meals = meals.annotate(avg_rating = Avg("mealrating__rating")).order_by('-avg_rating')
+#         elif q == "date":
+#             # new added
+#             meals = meals.order_by("-dateAdded")
+#         else:
+#             meals = meals.order_by("countryOfOrigin")
+
+#         return meals
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["category"] = self.kwargs["category"]
+#         return context
